bce9dc04d0804f2884a335add6bd2dc127d0945c
Muhammad Irvandi Lubis <113462342+muhammadirvandilubis@users.noreply.github.com>
2022-11-15 12:16:59 +0700

    Add files via upload

---
+++ penjualan/src/main/java/com/vandi/penjualan/VO/Pelanggan.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.vandi.penjualan.VO;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ *
+ * @author Vandi
+ */
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Pelanggan {
+    private Long pelangganKodeBarang;
+    private String NamaBarang;
+}
